8baa801aaa9424255cab608262c1a17e8d1b076a
J. A. Hernández <joseanher81@gmail.com>
2020-02-17 19:52:46 +0100

    Initialize Passport

---
+++ passport/index.js
@@ -0,0 +1,23 @@
+const passport = require("passport");
+const User = require("../models/user");
+
+//Require strategies  TODO
+require("./strategies/local");
+require("./strategies/facebook");
+
+// Serialize and Deserialize
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+  console.log("deserializing user");
+  User.findById(id)
+    .then(user => cb(null, user))
+    .catch(e => cb(err));
+});
+
+module.exports = app => {
+  app.use(passport.initialize());
+  app.use(passport.session());
+};
