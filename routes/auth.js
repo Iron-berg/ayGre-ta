f0d47f1348e574ef8cd2aa0e0c79a973fdfeee7f
Ana Segarra <anasegarraber@gmail.com>
2020-02-19 19:00:59 +0100

    Fix signup and local login routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,59 +1,55 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const passport = require("passport");
-const User = require("../models/user");
-const { hashPassword } = require("../lib/hashing");
+const passport = require('passport');
+const User = require('../models/user');
+const { hashPassword } = require('../lib/hashing');
 
 // Signup route
-router.post('/', async (req, res, next) => {
+router.post('/auth/signup', async (req, res, next) => {
 	try {
-		const { username, password, signup } = req.body;
-
-		if (!signup) return next();
-
+		const { username, password } = req.body;
 		const registeredUser = await User.findOne({ username });
 
-    if (registeredUser) {
-      console.log(`User ${registeredUser.username} already exists`);
-      return res.redirect("/");
-    } else {
-      const newUser = await User.create({
-        username,
-        password: hashPassword(password)
-      });
-      console.log(`${newUser} created`);
-      return res.redirect("/");
-    }
-  } catch (error) {
-    console.log("Credentials are necessary");
-    return res.redirect("/");
-  }
+		if (registeredUser) {
+			console.log(`User ${registeredUser.username} already exists`);
+			return res.redirect('/');
+		} else {
+			const newUser = await User.create({
+				username,
+				password: hashPassword(password)
+			});
+			console.log(`${newUser} created`);
+			return res.redirect('/');
+		}
+	} catch (error) {
+		console.log('Credentials are necessary');
+		return res.redirect('/');
+	}
 });
 
 // Facebook login
-router.get("/login/facebook", passport.authenticate("facebook"));
+router.get('/login/facebook', passport.authenticate('facebook'));
 
 router.get(
-  "/auth/facebook/callback",
-  passport.authenticate("facebook", {
-    failureRedirect: "/" // here will redirect to login page
-  }),
-  function(req, res) {
-    // Successful authentication, redirect private page.
-    console.log("Succesfully logged with Facebook");
-    res.redirect("/");
-  }
+	'/auth/facebook/callback',
+	passport.authenticate('facebook', {
+		failureRedirect: '/' // here will redirect to login page
+	}),
+	function(req, res) {
+		// Successful authentication, redirect private page.
+		console.log('Succesfully logged with Facebook');
+		res.redirect('/');
+	}
 );
-    
+
 // Local login
-router.post('/', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/' }));
+router.post('/auth/login', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/' }));
 
 // Logout route
-router.get("/logout", (req, res) => {
-  req.logout();
-  console.log("User logged out");
-  res.redirect("/");
+router.get('/logout', (req, res) => {
+	req.logout();
+	console.log('User logged out');
+	res.redirect('/');
 });
 
-    
 module.exports = router;
