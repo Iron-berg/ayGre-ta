d450332222498be79e35f08b2b71f97e8fdf6d2e
J. A. Hernández <joseanher81@gmail.com>
2020-02-22 09:32:40 +0100

    Added method in AirVisual Service for getting current temperature

--- services/airVisualService.js
+++ services/airVisualService.js
@@ -1,22 +1,34 @@
-const axios = require('axios');
+const axios = require("axios");
 
 class AirVisualService {
   constructor() {
     this.service = axios.create({
       baseURL:
-        'https://api.airvisual.com/v2/nearest_city?key=' +
+        "https://api.airvisual.com/v2/nearest_city?key=" +
         process.env.API_AIR_TOKEN
     });
   }
 
   getAirIndex() {
-    let result = '';
+    let result = "";
 
     return this.service
       .get()
       .then(response => {
         result = response.data;
         return result.data.current.pollution.aqius; // returns the current contamination index
+      })
+      .catch(error => console.log(`ERROR: ${error}`));
+  }
+
+  getTemperature() {
+    let result = "";
+
+    return this.service
+      .get()
+      .then(response => {
+        result = response.data;
+        return result.data.current.weather.tp; // returns the current temperature in Celsius
       })
       .catch(error => console.log(`ERROR: ${error}`));
   }
