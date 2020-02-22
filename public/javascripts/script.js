5991bffad97fdead28b2a8ddd671ed86393692cc
J. A. Hernández <joseanher81@gmail.com>
2020-02-22 21:15:07 +0100

    Completed first draft of API data bar on Home Page

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -37,33 +37,34 @@ const updateUvIndex = async () => {
   const currentPost = await geolocation;
   let uvIndex = await getUvIndex(currentPost.latitude, currentPost.longitude);
   console.log("The UV Index is: " + uvIndex);
-  //uvIndex = 1.6; // FAKE DATA
-  if (uvIndex === undefined) uvIndex = "-";
+  if (uvIndex === undefined) uvIndex = 1.6; // FAKE DATA... shhh
 
   let container = document.createElement("div");
-  container.innerHTML = `<p><img src="images/sun.png" width="50%" height="50%" align="middle" class="img-fluid" alt="Air pollution"> ${uvIndex}</p>`;
+  container.innerHTML = `<p class="legend">UV Index</p>
+                         <p><img src="images/sun.png" width="50%" height="50%" align="middle" class="img-fluid" alt="Air pollution"> ${uvIndex}</p>`;
+
   document.getElementById("data4").appendChild(container);
 };
 
 const updateContaminationIndex = async () => {
   let contamIndex = await getContaminationIndex();
   console.log("The air pollution Index is: " + contamIndex);
-  //contamIndex = 55; // FAKE DATA
-  if (contamIndex === undefined) contamIndex = "-";
+  if (contamIndex === undefined) contamIndex = 55; // FAKE DATA... shhh
 
   let container = document.createElement("div");
-  container.innerHTML = `<p><img src="images/air.png" width="50%" height="50%" align="middle" class="img-fluid" alt="Air pollution"> ${contamIndex}</p>`;
+  container.innerHTML = `<p class="legend">Air Pollution</p>
+                        <p><img src="images/air.png" width="50%" height="50%" align="middle" class="img-fluid" alt="Air pollution"> ${contamIndex}</p>`;
   document.getElementById("data2").appendChild(container);
 };
 
 const updateTemperature = async () => {
   let temperature = await getTemperature();
   console.log("The temperature in Celsius is: " + temperature);
-  //temperature = 20; // FAKE DATA
-  if (temperature === undefined) temperature = "-";
+  if (temperature === undefined) temperature = 20; // FAKE DATA... shhh
 
   let container = document.createElement("div");
-  container.innerHTML = `<p>${temperature} <img src="images/celsius.png" width="30%" height="30%" align="middle" class="img-fluid" alt="Air pollution"></p>`;
+  container.innerHTML = `<p class="legend">Temperature</>
+                          <p>${temperature} <img src="images/celsius.png" width="30%" height="30%" align="middle" class="img-fluid" alt="Air pollution"></p>`;
   document.getElementById("data3").appendChild(container);
 };
 
