432dd78ffc4aa0413aaf25260c64b017b361a814
Ana Segarra <anasegarraber@gmail.com>
2020-02-25 20:50:40 +0100

    Finish leaderboard styling

--- routes/private.js
+++ routes/private.js
@@ -6,20 +6,22 @@ const User = require('../models/user');
 // GET user private page
 router.get('/user', ensureLogin.ensureLoggedIn(), async (req, res, next) => {
 	try {
-		const users = await User.find();
-		const platformUsers = users
-			.map(user => {
-				return {
-					username: user.username,
-					followers: user.followers,
-					gretaPoints: user.gretaPoints,
-					likes: user.gretaPoints > 0 ? (user.gretaPoints - user.followers) / 2 : 0,
-					isCurrentUser: req.user.username === user.username ? true : false
-				};
-			})
-			.sort((a, b) => b.gretaPoints - a.gretaPoints);
+		const users = await User.find().sort({ gretaPoints: -1 });
+		const platformUsers = users.map((user, index) => {
+			return {
+				username: user.username,
+				followers: user.followers,
+				gretaPoints: user.gretaPoints,
+				likes: user.gretaPoints > 0 ? (user.gretaPoints - user.followers) / 2 : 0,
+				isCurrentUser: req.user.username === user.username ? true : false,
+				isTopUser: index < 5
+			};
+		});
 
-		res.render('user', { platformUser: platformUsers });
+		console.log(platformUsers);
+		const currentUser = platformUsers.find(user => user.username === req.user.username);
+
+		res.render('user', { platformUser: platformUsers.slice(0, 5), currentUser });
 	} catch (error) {
 		console.log(error);
 	}
