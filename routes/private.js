cb23c192f3a9061c128dcbcf28c52f73ef042a18
Ana Segarra <anasegarraber@gmail.com>
2020-02-25 02:59:55 +0100

    Generate dinamically leaderboard table

--- routes/private.js
+++ routes/private.js
@@ -1,10 +1,28 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const ensureLogin = require("connect-ensure-login");
+const ensureLogin = require('connect-ensure-login');
+const User = require('../models/user');
 
 // GET user private page
-router.get("/user", ensureLogin.ensureLoggedIn(), (req, res, next) => {
-  res.render("user");
+router.get('/user', ensureLogin.ensureLoggedIn(), async (req, res, next) => {
+	try {
+		const users = await User.find();
+		const platformUsers = users
+			.map(user => {
+				return {
+					username: user.username,
+					followers: user.followers,
+					gretaPoints: user.gretaPoints,
+					likes: user.gretaPoints > 0 ? (user.gretaPoints - user.followers) / 2 : 0,
+					isCurrentUser: req.user.username === user.username ? true : false
+				};
+			})
+			.sort((a, b) => b.gretaPoints - a.gretaPoints);
+
+		res.render('user', { platformUser: platformUsers });
+	} catch (error) {
+		console.log(error);
+	}
 });
 
 module.exports = router;
