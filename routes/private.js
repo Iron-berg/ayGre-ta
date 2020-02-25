829dff33c707d3f323862849bd76aa0f30b11dd7
J. A. Hernández <joseanher81@gmail.com>
2020-02-25 23:26:44 +0100

    Show thunbergs from user and followings

--- routes/private.js
+++ routes/private.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const ensureLogin = require("connect-ensure-login");
+const mongoThunbergService = require("../services/mongoThunbergService");
 
 // GET user private page
-router.get("/user", ensureLogin.ensureLoggedIn(), (req, res, next) => {
-  res.render("user");
+router.get("/user", ensureLogin.ensureLoggedIn(), async (req, res, next) => {
+  // Pre-load the thunbergs posts
+  // const thunbergs = [
+  //   { message: "This is a test", author: "José Angel", likes: "5" },
+  //   { message: "Soy una petarda", author: "Ana", likes: "3" },
+  //   { message: "Ola ke ase", author: "Lady Frameworks", likes: "1" }
+  // ];
+  const thunbergs = await mongoThunbergService.getRelatedThunbergs(req.user.id);
+  console.log(
+    "LA RESPESTA MONGOLA " + thunbergs + "TAMANO " + thunbergs.length
+  );
+
+  res.render("user", { thunbergs });
 });
 
 module.exports = router;
--- services/mongoThunbergService.js
+++ services/mongoThunbergService.js
@@ -13,20 +13,64 @@ class MongoThunbergService {
 
       // Create Thunberg
       const thunberg = await this.Thunberg.create(t);
-      console.log(
-        `MongoThunbergService postThunberg - Thunberg created ${thunberg}`
-      );
 
       // Add Thunberg to user
       const currentUser = await this.User.findById(author);
       currentUser.thunbergs.push(thunberg);
       currentUser.save();
-      console.log(
-        `MongoThunbergService postThunberg - Thunberg saved to user ${currentUser}`
-      );
     } catch (error) {
       console.log(`ERROR MongoThunbergService postThunberg ${error}`);
     }
+  };
+
+  getRelatedThunbergs = async function(user) {
+    try {
+      let allThunbergs = [];
+
+      // Get logged user thunbergs
+      const thunbergsUser = await this.User.findById(user).populate({
+        path: "thunbergs",
+        model: this.Thunberg,
+        populate: {
+          path: "author",
+          model: this.User
+        }
+      });
+      allThunbergs.push(...thunbergsUser.thunbergs);
+
+      // Get followings from user
+      const followings = await this.User.findById(user).populate("followings");
+
+      // Get thunbergs from each following
+      const followingThunbergs = await this._getThunbergsFromUsers(
+        followings.followings
+      );
+
+      if (followingThunbergs.length > 0)
+        allThunbergs.push(...followingThunbergs);
+
+      return allThunbergs;
+    } catch (error) {
+      console.log(`ERROR MongoThunbergService getRelatedThunbergs ${error}`);
+    }
+  };
+
+  _getThunbergsFromUsers = async function(users) {
+    let totalThunbergs = [];
+    for (let i = 0; i < users.length; i++) {
+      let usr = await this.User.findById(users[i].id).populate({
+        path: "thunbergs",
+        model: this.Thunberg,
+        populate: {
+          path: "author",
+          model: this.User
+        }
+      });
+
+      totalThunbergs.push(...usr.thunbergs);
+    }
+
+    return totalThunbergs;
   };
 }
 
