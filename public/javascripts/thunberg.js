28a920728db6d0955138feef54cd3de41a45d001
J. A. Hernández <joseanher81@gmail.com>
2020-02-26 19:45:17 +0100

    Thunberg onclick

--- public/javascripts/thunberg.js
+++ public/javascripts/thunberg.js
@@ -10,3 +10,23 @@ document
     await postThunberg(message, logedUser);
     document.getElementById("message").value = "";
   });
+
+// Like Thunberg
+// document.getElementById("like").addEventListener("click", async function() {
+//   // const thunbergid = document
+//   //   .getElementById("like")
+//   //   .getAttribute("data-thunbergid");
+//   console.log("EL THUNBERG ID ES ");
+
+//   //await likehunberg(thunbergid);
+// });
+
+// Like Thunberg
+const onLikeClick = async function(id) {
+  console.log("EL THUNBERG ID ES" + id);
+};
+
+// document.getElementsByClassName("like").addEventListener("click", function(e) {
+//   console.log("HOLA");
+//   console.log("LLEGA " + e.target.getAttribute("data-thunbergid"));
+// });
