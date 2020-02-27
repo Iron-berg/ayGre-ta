1ed82a381804231dd6c330dcefa519750742b231
J. A. Hernández <joseanher81@gmail.com>
2020-02-28 00:05:53 +0100

    Fixed problem with FB callback

--- passport/strategies/facebook.js
+++ passport/strategies/facebook.js
@@ -7,7 +7,7 @@ passport.use(
     {
       clientID: process.env.FACEBOOK_APP_ID,
       clientSecret: process.env.FACEBOOK_APP_SECRET,
-      callbackURL: "http://localhost:3000/auth/facebook/callback",
+      callbackURL: process.env.FACEBOOK_CALLBACK,
       profileFields: ["id", "displayName", "photos"]
     },
 
