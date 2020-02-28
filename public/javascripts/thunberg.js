62a4fd2ac030f6943ca30b3db9775746d57f14cc
J. A. Hernández <joseanher81@gmail.com>
2020-02-28 18:04:01 +0100

    Added cancel button funcionality and some js fixes

--- public/javascripts/thunberg.js
+++ public/javascripts/thunberg.js
@@ -31,21 +31,28 @@ document.getElementById("btnThunberg").addEventListener("click", onPostClick);
 
 // Message character lenght control
 document.getElementById("message").onkeyup = function() {
-  const buttonPost = document.getElementById("btnThunberg");
-  const charcounter = document.getElementById("characters-left");
-  charcounter.innerHTML = 50 - this.value.length;
+  updateFormStatus();
+};
 
-  /*   if (parseInt(charcounter.innerHTML) < 0) {
-    charcounter.classList.add("red");
-    buttonPost.disabled = true;
-  } else {
-    charcounter.classList.remove("red");
-    buttonPost.disabled = false;
-  } */
-  this.value.length > 50
+// Cancel button clears text area
+document.getElementById("btnCancel").addEventListener("click", function() {
+  document.getElementById("message").value = "";
+  updateFormStatus();
+});
+
+const updateFormStatus = function() {
+  const buttonPost = document.getElementById("btnThunberg");
+  const messageArea = document.getElementById("message");
+  const charcounter = document.getElementById("characters-left");
+
+  charcounter.innerHTML = 50 - messageArea.value.length;
+
+  messageArea.value.length > 50
     ? charcounter.classList.add("red")
     : charcounter.classList.remove("red");
 
   buttonPost.disabled =
-    this.value.length == 0 || this.value.length > 50 ? true : false;
+    messageArea.value.length == 0 || messageArea.value.length > 50
+      ? true
+      : false;
 };
