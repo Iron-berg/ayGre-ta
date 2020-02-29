345739825631b852d050f3b76712368fc807cd56
Ana Segarra <anasegarraber@gmail.com>
2020-03-01 00:53:33 +0100

    Remove unnecessary console.log

--- app.js
+++ app.js
@@ -1,108 +1,102 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const express = require("express");
-const favicon = require("serve-favicon");
-const hbs = require("hbs");
-const mongoose = require("mongoose");
-const logger = require("morgan");
-const path = require("path");
-const session = require("express-session");
-const MongoStore = require("connect-mongo")(session);
-const flash = require("connect-flash");
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const flash = require('connect-flash');
 
 mongoose
-  .connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-  })
-  .then(x => {
-    console.log(
-      `Connected to Mongo! Database name: "${x.connections[0].name}"`
-    );
-  })
-  .catch(err => {
-    console.error("Error connecting to mongo", err);
-  });
+	.connect(process.env.MONGODB_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true
+	})
+	.then(x => {
+		console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+	})
+	.catch(err => {
+		console.error('Error connecting to mongo', err);
+	});
 
-const app_name = require("./package.json").name;
-const debug = require("debug")(
-  `${app_name}:${path.basename(__filename).split(".")[0]}`
-);
+const app_name = require('./package.json').name;
+const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
 const app = express();
 
 // Middleware Setup
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(
-  session({
-    secret: "claudia cat",
-    resave: true,
-    saveUninitialized: true,
-    store: new MongoStore({ mongooseConnection: mongoose.connection })
-  })
+	session({
+		secret: 'claudia cat',
+		resave: true,
+		saveUninitialized: true,
+		store: new MongoStore({ mongooseConnection: mongoose.connection })
+	})
 );
 app.use(flash());
 
-require("./passport")(app); // This automatically requires index.js in this folder
+require('./passport')(app); // This automatically requires index.js in this folder
 
 // Express View engine setup
 
 app.use(
-  require("node-sass-middleware")({
-    src: path.join(__dirname, "public"),
-    dest: path.join(__dirname, "public"),
-    sourceMap: true,
-    outputStyle: "compressed"
-  })
+	require('node-sass-middleware')({
+		src: path.join(__dirname, 'public'),
+		dest: path.join(__dirname, 'public'),
+		sourceMap: true,
+		outputStyle: 'compressed'
+	})
 );
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "hbs");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 hbs.registerPartials(`${__dirname}/views/partials`);
-app.use(express.static(path.join(__dirname, "public")));
-app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 // HBS helpers
-hbs.registerHelper("trimDate", function(passedString) {
-  var theString = String(passedString).substring(4, 15);
-  return new hbs.SafeString(theString);
+hbs.registerHelper('trimDate', function(passedString) {
+	var theString = String(passedString).substring(4, 15);
+	return new hbs.SafeString(theString);
 });
 
 // HBS helpers
 hbs.registerHelper('trimName', str => {
 	let normalizedStr = str.trim();
 	if (!normalizedStr.includes(' ')) {
-		console.log('no spaces');
 		return str;
 	}
 	const trimmed = normalizedStr.split(' ');
-	console.log(trimmed);
 	return `${trimmed[0]} ${trimmed[1][0]}.`;
 });
 
 // default value for title local
-app.locals.title = "ayGre-ta";
+app.locals.title = 'ayGre-ta';
 
 app.use((req, res, next) => {
-  res.locals.user = req.user;
-  console.log("USUARIO" + req.user);
-  next();
+	res.locals.user = req.user;
+	console.log('USUARIO' + req.user);
+	next();
 });
 
 // Flash alerts
 app.use(function(req, res, next) {
-  res.locals.error = req.flash("error");
-  res.locals.info = req.flash("info");
-  next();
+	res.locals.error = req.flash('error');
+	res.locals.info = req.flash('info');
+	next();
 });
 
-const index = require("./routes/index");
-app.use("/", index);
+const index = require('./routes/index');
+app.use('/', index);
 
 module.exports = app;
